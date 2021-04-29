8f000f856144c7e2b15ba1ec6c1e0f67cf5dda67
Tyler Kerns <tkerns@protonmail.com>
2021-04-29 14:19:17 -0500

    structure test site

---
+++ scripts/main.js
@@ -0,0 +1,58 @@
+
+/* This is a 
+   Multi-line
+   Comment
+*/
+
+// Comment
+
+let iceCream = 'chocolate';
+if(iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');
+} else {
+  alert('Awwww, but chocolate is my favorite...');
+}
+
+function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result;
+  }
+
+  document.querySelector('h1').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/patches1.jpeg') {
+      myImage.setAttribute('src','images/patches2.jpeg');
+    } else {
+      myImage.setAttribute('src','images/patches1.jpeg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }

9d4f87b0c020eb51f242299378b0b5c3bce4ffee
Samuel Frost <samuelfrost@Samuels-Mac-mini.local>
2021-05-18 17:10:49 -0700

    NOW megamenu links lead to under-construction page

--- components/header.js
+++ components/header.js
@@ -19,40 +19,40 @@ class Header extends HTMLElement {
                         <h3>Green Circle <span class='difficulty-symbol green'>●</span></h3>
                         <ul>
                             <li><a href='/pages/puzzles/agta.html'>Agta</a></li>
-                            <li><a>Chickasaw</a></li>
-                            <li><a>Kurdish</a></li>
-                            <li><a>Nahuatl</a></li>
-                            <li><a>Swahili</a></li>
-                            <li><a>Tajik</a></li>
-                            <li><a>Turkish</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Chickasaw</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Kurdish</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Nahuatl</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Swahili</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Tajik</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Turkish</a></li>
                             <li><a href='/pages/puzzles/yaqui.html'>Yaqui</a></li>
                         </ul>
                     </div>
                     <div class="megamenu-col">
                         <h3>Blue Square <span class='difficulty-symbol blue'>■</span></h3>
                         <ul>
-                            <li><a>Archi</a></li>
-                            <li><a>Czech</a></li>
-                            <li><a>Endo</a></li>
-                            <li><a>French</a></li>
-                            <li><a>Hausa</a></li>
-                            <li><a>Hawaiian</a></li>
-                            <li><a>Indonesian</a></li>
-                            <li><a>Luvian</a></li>
-                            <li><a href=''>Maasai</a></li>
-                            <li><a href=''>Quechua</a></li>
-                            <li><a href=''>Sanskrit</a></li>
-                            <li><a href=''>Shugnan</a></li>
-                            <li><a href=''>Swahili</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Archi</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Czech</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Endo</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>French</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Hausa</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Hawaiian</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Indonesian</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Luvian</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Maasai</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Quechua</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Sanskrit</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Shugnan</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Swahili</a></li>
                         </ul>
                     </div>
                     <div class="megamenu-col">
                         <h3>Black Diamond <span class='difficulty-symbol black'>◆</span></h3>
                         <ul>
-                            <li><a>Babylonian</a></li>
-                            <li><a>Georgian</a></li>
-                            <li><a>Orkhono-Yeniseyan</a></li>
-                            <li><a>Samoan</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Babylonian</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Georgian</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Orkhono-Yeniseyan</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Samoan</a></li>
                             <li class='megamenu-padding'> </li>
                         </ul>
 
@@ -60,10 +60,10 @@ class Header extends HTMLElement {
 
                         <h3>Double Black Diamond <span class='difficulty-symbol black'>◆◆</span></h3>
                         <ul>
-                            <li><a>Amharic</a></li>
-                            <li><a>Ancient Inscriptions</a></li>
-                            <li><a>Thai</a></li>
-                            <li><a>Tocharian</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Amharic</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Ancient Inscriptions</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Thai</a></li>
+                            <li><a href='/pages/puzzles/under-construction.html'>Tocharian</a></li>
                         </ul>
                     </div>
                 </div>

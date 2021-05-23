6791ced29d04d453f036eada5dab98ebba569dc9
Samuel Frost <samuelfrost@Samuels-Mac-mini.local>
2021-05-22 19:29:58 -0700

    Added turkish puzzle

---
+++ scripts/puzzle-scripts/turkish.js
@@ -0,0 +1,37 @@
+let q1 = document.getElementById('q1');
+let q2 = document.getElementById('q2');
+let q3 = document.getElementById('q3');
+let q4 = document.getElementById('q4');
+let q5 = document.getElementById('q5');
+
+let answers = ['denizde', 'masalar', 'evlere', 'havuça', 'defterde'];
+let accuracy = [false, false, false, false, false];
+let userInputs = [q1, q2, q3, q4, q5];
+
+let correct = 0;
+
+let progressBar = document.getElementById('correct');
+let progressOutput = document.getElementById('num-correct');
+
+function checkAnswers() {
+    for (i = 0; i < userInputs.length; i++) {
+        if (userInputs[i].value === answers[i]) {
+            accuracy[i] = true;
+        }
+    }
+    
+    tallyCorrect();
+
+    progressOutput.textContent = correct;
+    progressBar.value = correct;
+}
+
+function tallyCorrect() {
+    correct = 0;
+
+    for(i = 0 ; i < 5 ; i++) {
+        if (accuracy[i]) {
+            correct++;
+        }
+    }
+}
